3163822a5a0cef03e0fa22d6629fe99725a147aa
iamyajat <myajat21@gmail.com>
2020-12-12 23:02:24 +0530

    HTML extractor added

--- popup.js
+++ popup.js
@@ -1,7 +1,9 @@
+var i = 0;
 document.addEventListener('DOMContentLoaded', function() {
   var checkPageButton = document.getElementById('checkPage');
   checkPageButton.addEventListener('click', function() {
-    document.getElementById("demo").innerHTML = "Hello World";
+    document.getElementById("demo").innerHTML = "Count: "+i;
+    i++;
     // chrome.tabs.getSelected(null, function(tab) {
     //   d = document;
 
@@ -18,3 +20,26 @@ document.addEventListener('DOMContentLoaded', function() {
     // });
   }, false);
 }, false);
+
+chrome.runtime.onMessage.addListener(function(request, sender) {
+  if (request.action == "getSource") {
+    message.innerText = request.source;
+  }
+});
+
+function onWindowLoad() {
+
+  var message = document.querySelector('#message');
+
+  chrome.tabs.executeScript(null, {
+    file: "getPagesSource.js"
+  }, function() {
+    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+    if (chrome.runtime.lastError) {
+      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+    }
+  });
+
+}
+
+window.onload = onWindowLoad;
