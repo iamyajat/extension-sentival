79a80874388efc5abad13128a44d5466ae460ba2
iamyajat <myajat21@gmail.com>
2020-12-13 09:02:43 +0530

    Added para extractor

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-var i = 0;
+var i = 1;
 document.addEventListener('DOMContentLoaded', function() {
   var checkPageButton = document.getElementById('checkPage');
   checkPageButton.addEventListener('click', function() {
@@ -27,19 +27,19 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
   }
 });
 
-function onWindowLoad() {
+// function onWindowLoad() {
 
-  var message = document.querySelector('#message');
+//   var message = document.querySelector('#message');
 
-  chrome.tabs.executeScript(null, {
-    file: "getPagesSource.js"
-  }, function() {
-    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
-    if (chrome.runtime.lastError) {
-      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
-    }
-  });
+//   chrome.tabs.executeScript(null, {
+//     file: "getPagesSource.js"
+//   }, function() {
+//     // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+//     if (chrome.runtime.lastError) {
+//       message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+//     }
+//   });
 
-}
+// }
 
-window.onload = onWindowLoad;
+// window.onload = onWindowLoad;
